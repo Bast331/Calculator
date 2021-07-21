47f17a1cf4caa0e802f563611b4fb01ec9e38194
Bast <neubeli@web.de>
2021-07-21 09:14:40 +0200

    ﻿Create Event Listener and make Display from Calculator work created event listner for all buttons and let all numbers show in the display. Store the value of the display each time a number is pressed in a variable.

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-let result = 0;
-let number1 = 0;
-let number2 = 0;
+let result;
+let number1;
+let number2;
 let operator;
-let a = 0;
-let b = 0;
+let a;
+let b;
+let displayvalue;
 
-
+// functions that make the calculation and give back the results
 function addition(number1, number2) {
 return result = number1 + number2;
 }
@@ -23,6 +24,7 @@ return result = number1 / number2;
 }
 
 
+// function that gets the numbers and calls the rigth calculating function
 function operate(a, b, operator) {
     console.log(`A is ${a}`);
     console.log(`B is ${b}`);
@@ -62,15 +64,106 @@ function operate(a, b, operator) {
 }
 
 
+// Eventlisteners for all buttons
+const Display = document.querySelector("#displayShow");
 
-// function function_one()
-// {
-//     alert("The function called 'function_one' has been called.")
-//     //Here u would like to call function_two.
-//     function_two(g); 
-// }
+document.querySelector(".button_Clear").addEventListener("click", () => {
+    console.log("Clear pressed")
+})
 
-// function function_two()
-// {
-//     alert("The function called 'function_two' has been called.")
-// }
+document.querySelector(".button_Back").addEventListener("click", () => {
+    console.log("Back pressed");
+})
+
+document.querySelector(".button_Plus-minus").addEventListener("click", () => {
+    console.log("plus-minus pressed");
+})
+
+document.querySelector(".button_divide").addEventListener("click", () => {
+    console.log("divide pressed");
+})
+
+document.querySelector(".button_multiply").addEventListener("click", () => {
+    console.log("multiply pressed");
+})
+
+document.querySelector(".button_subtract").addEventListener("click", () => {
+    console.log("subtrace pressed");
+})
+
+document.querySelector(".button_plus").addEventListener("click", () => {
+    console.log("plus pressed");
+})
+
+document.querySelector(".button_equal").addEventListener("click", () => {
+    console.log("equal pressed");
+})
+
+document.querySelector(".button_point").addEventListener("click", () => {
+    console.log("point pressed");
+})
+
+document.querySelector(".button_zero").addEventListener("click", () => {
+    console.log("0 pressed");
+    displayvalue = Display.innerHTML += (0);
+    console.log(displayvalue);
+})
+
+document.querySelector(".button_one").addEventListener("click", () => {
+    console.log("1 pressed");
+    displayvalue = Display.innerHTML += (1);
+    console.log(displayvalue);
+})
+
+document.querySelector(".button_two").addEventListener("click", () => {
+    console.log("2 pressed");
+    displayvalue = Display.innerHTML += (2);
+    console.log(displayvalue);
+})
+
+document.querySelector(".button_three").addEventListener("click", () => {
+    console.log("3 pressed");
+    displayvalue = Display.innerHTML += (3);
+    console.log(displayvalue);
+})
+
+document.querySelector(".button_four").addEventListener("click", () => {
+    console.log("4 pressed");
+    displayvalue = Display.innerHTML += (4);
+    console.log(displayvalue);
+})
+
+document.querySelector(".button_five").addEventListener("click", () => {
+    console.log("5 pressed");
+    displayvalue = Display.innerHTML += (5);
+    console.log(displayvalue);
+})
+
+document.querySelector(".button_six").addEventListener("click", () => {
+    console.log("6 pressed");
+    displayvalue = Display.innerHTML += (6);
+    console.log(displayvalue);
+})
+
+document.querySelector(".button_seven").addEventListener("click", () => {
+    console.log("7 pressed");
+    displayvalue = Display.innerHTML += (7);
+    console.log(displayvalue);
+})
+
+document.querySelector(".button_eight").addEventListener("click", () => {
+    console.log("8 pressed");
+    displayvalue = Display.innerHTML += (8);
+    console.log(displayvalue);
+})
+
+document.querySelector(".button_nine").addEventListener("click", () => {
+    console.log("9 pressed");
+    displayvalue = Display.innerHTML += (9);
+    console.log(displayvalue);
+})
+
+// function that gets called by eventlistener to update calc display
+function calcDisplay() {
+   
+}
