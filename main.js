c9911d1cda6de79ec8b02c5f48f57ecce3cc9225
Bast <neubeli@web.de>
2021-07-24 17:32:31 +0200

    ﻿All functions of the calculator work, there are no known bugs anymore. For the problem with the calculation by pressing no equal but an operator again I looked inside the solution from Mr_Starkk on github. (https://github.com/Mr-Starkk/Calculator/blob/main/calc.js) and included the logic into my project. I also made my Code look cleaner and shorter. All other buttons can be used: Clear, Backspace, to neg., point. The button that should transform negative numbers into positives and vice versa now „only“ sets a minus at the start of a number. But I think that is enough to cover many calculations.

--- main.js
+++ main.js
@@ -1,169 +1,219 @@
-let result;
-let number1;
-let number2;
-let operator;
-let a;
-let b;
-let displayvalue;
+// declare all variables (to null, so the different scenarios are solved: iif null then calculator behaves like it 
+// just loaded)
+let result = null;
+let number1 = null;
+let number2 = null;
+let operator = null;
 
-// functions that make the calculation and give back the results
-function addition(number1, number2) {
-return result = number1 + number2;
+// if an operator/ equal is clicked the screen on Display should be cleared
+let displayNew = true;
+// dont allow multiple points (.) in one number
+let point = false;
+// to set minus before a number, but you shouldnt overwrite the whole screen if you wrote a number and then pressed the neg button
+let neg = false;
+
+
+const Display = document.getElementById("displayShow");
+const buttons = document.getElementById("buttonContainer");
+const equal = document.getElementById("equal");
+
+const clear = document.querySelector(".button_Clear");
+const back = document.querySelector(".button_Back");
+const dotButton = document.querySelector('.button_point');
+const negativeNumber = document.querySelector('.button_toMinus');
+
+
+function addition(a, b) {
+    result = a + b;
+    return result;
+
 }
 
-function subtraction(number1, number2) {
-return result = number1 - number2;
+function subtraction(a, b) {
+    result = a - b;
+    return result;
+    
 }
 
-function multiply(number1, number2) {
-return result = number1 * number2;
+function multiplication(a, b) {
+    result = a * b;
+    return result = Math.round((a*b)*1000000) / 1000000;
+  
 }
 
-function division(number1, number2) {
-return result = number1 / number2;
+function division(a, b) {
+    result = a / b;
+    return result = Math.round((a/b)*1000000) / 1000000;
 }
 
 
-// function that gets the numbers and calls the rigth calculating function
-function operate(a, b, operator) {
-    console.log(`A is ${a}`);
-    console.log(`B is ${b}`);
-    console.log(operator);
-    number1 = a;
-    console.log(number1);
-    number2 = b; 
-    console.log(number2);
 
+function operate(operator, numberOne, numberTwo) {
 
     if(operator == "+") {
-        
-        return addition(number1, number2);
+        return addition(numberOne, numberTwo);
     }
-   
     else if(operator == "-") {
-       
-        return subtraction(number1, number2);
-    } 
-
+        return subtraction(numberOne, numberTwo);
+    }
     else if(operator == "*") {
-    
-
-        return multiply(number1, number2);
+        return multiplication(numberOne, numberTwo);
     }
-
     else if(operator == "/") {
-    
-
-        return division(number1, number2);
+        return division(numberOne, numberTwo);
     }
-    
     else {
-        return console.log("There must have been a mistake!");
+        console.log("ERROR")
+        Display.textContent = "ERROR";
+    }
+}
+
+
+buttons.querySelectorAll(".digit").forEach((button) => {
+    button.addEventListener("click", () => {
+        // when you need to clear the display (after operator or equal)
+        if(displayNew == true) {
+
+            // when you clicked equal, set all vars to the initial situation
+            if(number1 === undefined) {
+
+                // dont overwrite the - if it should be a neg number at the start of calculation (after equal)
+                if(Display.textContent == '-') {
+                    Display.textContent = Display.textContent;
+                }
+                else{
+                    Display.textContent = '';
+                }
+                
+                number1 = null;
+                number2 = null;
+                operator = null;
+                result = null;
+            }
+
+            // dont overwrite the - if it should be a neg number at the start of calculation
+            if(Display.textContent == '-') {
+                Display.textContent = Display.textContent;
+            }
+
+            else {
+                Display.textContent = '';
+            }
+        
+        displayNew = false;
+        }
+         
+        Display.textContent += `${button.value}`;
+    })
+})
+
+
+buttons.querySelectorAll(".operator").forEach((button) => {
+    button.addEventListener("click", () => {
+        // set point to false so after an operation is pressed you can use the point once again
+        point = false;
+
+        // if number1 is a real number (not after initial situation or after pressed equal) declare the shown number as number2
+        if(number1 !== null) {
+
+            // if the operator is pressed directly after equal calculate with result as number1
+            if(number1 == undefined) {
+                operator = `${button.value}`;
+                number1 = result;
+                neg = false;
+                return
+            }
+            
+        number2 = parseFloat(Display.textContent);
+        result = operate(operator, number1, number2);
+        Display.textContent = result;
+        number1 = result;
+        number2 = null;
+        operator = `${button.value}`;
+        displayNew= true;
+        neg = false;
+        return
+        }
+
+        
+        number1 = parseFloat(Display.textContent);
+        operator = `${button.value}`;
+        displayNew = true;
+        neg = false;
+    })
+})
+
+
+equal.addEventListener("click", () => {
+    if(operator == null) {
+        neg = false;
+        return
+    }
+    point = false;
+    number2 = parseFloat(Display.textContent);
+    result = operate(operator, number1, number2);
+    Display.textContent = result;
+    number1 = undefined;
+    number2 = null;
+    operator = null;
+    displayNew = true;
+    neg = false;
+})
+
+// event Listener for clearing the Screen: set all variables to initial situation
+clear.addEventListener("click", () => {
+    Display.textContent = '';
+    result = null;
+    number1 = null;
+    number2 = null;
+    operator = null;
+    return
+})
+
+
+back.addEventListener("click", () => {
+    Display.textContent = Display.textContent.slice(0, -1);
+})
+
+dotButton.addEventListener("click", () => {
+    // solution I found in another project, it doesnt solves the problem when the first button pressed after loading
+    // the calculator is ., that it shows a .
+    // if(Display.textContent.indexOf('.') !== -1) return;
+
+    // my own solution
+    if(point == true) {
+    return
+    }
+    else if(Display.textContent == ''){
+        return
+    }
+    else if(operator !== null && Display.textContent == number1) {
+        return
     }
 
-}
-
-
-// Eventlisteners for all buttons
-const Display = document.querySelector("#displayShow");
-
-document.querySelector(".button_Clear").addEventListener("click", () => {
-    console.log("Clear pressed")
+    Display.textContent = `${Display.textContent}.`
+    point = true;
 })
 
-document.querySelector(".button_Back").addEventListener("click", () => {
-    console.log("Back pressed");
-})
 
-document.querySelector(".button_Plus-minus").addEventListener("click", () => {
-    console.log("plus-minus pressed");
-})
 
-document.querySelector(".button_divide").addEventListener("click", () => {
-    console.log("divide pressed");
-})
-
-document.querySelector(".button_multiply").addEventListener("click", () => {
-    console.log("multiply pressed");
-})
-
-document.querySelector(".button_subtract").addEventListener("click", () => {
-    console.log("subtrace pressed");
-})
-
-document.querySelector(".button_plus").addEventListener("click", () => {
-    console.log("plus pressed");
-})
-
-document.querySelector(".button_equal").addEventListener("click", () => {
-    console.log("equal pressed");
-})
-
-document.querySelector(".button_point").addEventListener("click", () => {
-    console.log("point pressed");
-})
-
-document.querySelector(".button_zero").addEventListener("click", () => {
-    console.log("0 pressed");
-    displayvalue = Display.innerHTML += (0);
-    console.log(displayvalue);
-})
-
-document.querySelector(".button_one").addEventListener("click", () => {
-    console.log("1 pressed");
-    displayvalue = Display.innerHTML += (1);
-    console.log(displayvalue);
-})
-
-document.querySelector(".button_two").addEventListener("click", () => {
-    console.log("2 pressed");
-    displayvalue = Display.innerHTML += (2);
-    console.log(displayvalue);
-})
-
-document.querySelector(".button_three").addEventListener("click", () => {
-    console.log("3 pressed");
-    displayvalue = Display.innerHTML += (3);
-    console.log(displayvalue);
-})
-
-document.querySelector(".button_four").addEventListener("click", () => {
-    console.log("4 pressed");
-    displayvalue = Display.innerHTML += (4);
-    console.log(displayvalue);
-})
-
-document.querySelector(".button_five").addEventListener("click", () => {
-    console.log("5 pressed");
-    displayvalue = Display.innerHTML += (5);
-    console.log(displayvalue);
-})
-
-document.querySelector(".button_six").addEventListener("click", () => {
-    console.log("6 pressed");
-    displayvalue = Display.innerHTML += (6);
-    console.log(displayvalue);
-})
-
-document.querySelector(".button_seven").addEventListener("click", () => {
-    console.log("7 pressed");
-    displayvalue = Display.innerHTML += (7);
-    console.log(displayvalue);
-})
-
-document.querySelector(".button_eight").addEventListener("click", () => {
-    console.log("8 pressed");
-    displayvalue = Display.innerHTML += (8);
-    console.log(displayvalue);
-})
-
-document.querySelector(".button_nine").addEventListener("click", () => {
-    console.log("9 pressed");
-    displayvalue = Display.innerHTML += (9);
-    console.log(displayvalue);
-})
-
-// function that gets called by eventlistener to update calc display
-function calcDisplay() {
+// before clicking on the first digit (after start, operator or equal) you can set this number to a negative number
+negativeNumber.addEventListener("click", () => {
+    // start situation
+    if(Display.textContent == '') {
+        Display.textContent = '-';
+        return 
+    }
+    // after operator or equal is pressed you should be able to set next number negative
+    else if(operator !== null && neg == false || result !== null && neg == false) {
+        if(Display.textContent == number1 || Display.textContent == result) {
+            Display.textContent = '-';
+            neg = true;
+            return 
+        }
+       else {
+           return
+       }
+    }
    
-}
+})
